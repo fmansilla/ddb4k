4e21364780426b1c50910ee7115f67625e354b6e
fernando.mansilla <fernando.mansilla@etermax.com>
2020-04-18 15:56:20 -0300

    Kotlin versions updated to 1.3.70; Junit updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.21"
+    kotlin("jvm") version "1.3.70"
     `maven-publish`
 }
 
 object Versions {
-    const val KOTLIN = "1.3.21"
-    const val JUNIT = "5.4.2"
+    const val JUNIT = "5.5.2"
 }
 
 repositories {
@@ -18,9 +17,9 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8", Versions.KOTLIN))
-    implementation(kotlin("reflect", Versions.KOTLIN))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0-RC")
+    implementation(kotlin("stdlib", org.jetbrains.kotlin.config.KotlinCompilerVersion.VERSION))
+    implementation(kotlin("reflect", org.jetbrains.kotlin.config.KotlinCompilerVersion.VERSION))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
 
     implementation("software.amazon.awssdk:dynamodb:2.7.2")
 
@@ -36,15 +35,16 @@ tasks.withType<KotlinCompile> {
 }
 
 dependencies {
-    testImplementation("org.assertj:assertj-core:3.11.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:${Versions.JUNIT}")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:${Versions.JUNIT}")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:${Versions.JUNIT}")
+    testImplementation("org.assertj:assertj-core:3.15.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.5")
 
     testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.1.0")
 
     testImplementation("org.testcontainers:testcontainers:1.12.5")
     testImplementation("org.testcontainers:junit-jupiter:1.12.5")
-    //testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.0-M1")
 }
 
 val compileTestKotlin by tasks.getting(KotlinCompile::class) {
@@ -79,9 +79,8 @@ publishing {
 
 fun DependencyHandlerScope.loggingDependencies() {
     implementation("org.slf4j:slf4j-api:1.7.26")
-//    implementation("ch.qos.logback:logback-classic:1.1.3")
 
-    implementation("org.apache.logging.log4j:log4j-api:2.12.0")
-    implementation("org.apache.logging.log4j:log4j-core:2.12.0")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.12.0")
+    runtime("org.apache.logging.log4j:log4j-api:2.12.0")
+    runtime("org.apache.logging.log4j:log4j-core:2.12.0")
+    runtime("org.apache.logging.log4j:log4j-slf4j-impl:2.12.0")
 }
